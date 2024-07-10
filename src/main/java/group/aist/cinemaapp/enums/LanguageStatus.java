155e761eef6d45bf358a379db57c09c875f55698
selminazkerimli <47450005+kselminaz@users.noreply.github.com>
2024-07-10 15:24:25 +0400

    Feat: adding Language Entity and Repo

---
+++ src/main/java/group/aist/cinemaapp/enums/LanguageStatus.java
@@ -0,0 +1,13 @@
+package group.aist.cinemaapp.enums;
+
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+
+@Getter
+@RequiredArgsConstructor
+public enum LanguageStatus {
+    VISIBLE(1),UNVISIBLE(2),DELETED(3);
+
+    private final int value;
+
+}
---
+++ src/main/java/group/aist/cinemaapp/model/Language.java
@@ -0,0 +1,58 @@
+package group.aist.cinemaapp.model;
+
+import jakarta.persistence.*;
+import lombok.*;
+import lombok.experimental.FieldDefaults;
+import org.hibernate.annotations.CreationTimestamp;
+import org.hibernate.annotations.UpdateTimestamp;
+
+import java.time.LocalDateTime;
+import java.util.Objects;
+
+import static jakarta.persistence.GenerationType.IDENTITY;
+import static lombok.AccessLevel.PRIVATE;
+
+@Entity
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@Table(name = "languages")
+@Builder
+@FieldDefaults(level = PRIVATE)
+public class Language {
+
+    @Id
+    @GeneratedValue(strategy = IDENTITY)
+    Long id;
+
+    String isoCode;
+
+    String title;
+
+    Integer status;
+
+    @CreationTimestamp
+    LocalDateTime createdAt;
+
+    @UpdateTimestamp
+    LocalDateTime updatedAt;
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Language language = (Language) o;
+        return Objects.equals(getId(), language.getId());
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(getId());
+    }
+
+
+
+
+
+}
