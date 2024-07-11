5ae571c4452e7d1572d0d1c04a50b7ece5bb47bc
selminazkerimli <47450005+kselminaz@users.noreply.github.com>
2024-07-11 12:44:37 +0400

    Feat: add Language Service methods

--- src/main/java/group/aist/cinemaapp/mapper/LanguageMapper.java
+++ src/main/java/group/aist/cinemaapp/mapper/LanguageMapper.java
@@ -1,4 +1,40 @@
 package group.aist.cinemaapp.mapper;
 
+import group.aist.cinemaapp.dto.request.LanguageRequest;
+import group.aist.cinemaapp.dto.request.LanguageUpdateRequest;
+import group.aist.cinemaapp.dto.response.LanguageResponse;
+import group.aist.cinemaapp.dto.response.PageableResponse;
+import group.aist.cinemaapp.enums.LanguageStatus;
+import group.aist.cinemaapp.model.Language;
+import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
+import org.mapstruct.Named;
+import org.springframework.data.domain.Page;
+
+import java.util.List;
+
+@Mapper(componentModel = "spring",uses={LanguageStatus.class})
 public interface LanguageMapper {
+
+    Language toEntity(LanguageRequest request );
+
+    @Mapping(target = "status", source = "status", qualifiedByName = "getById")
+    LanguageResponse toResponse(Language entity);
+
+    @Mapping(target = "data", source = "content", qualifiedByName = "getLanguageList")
+    @Mapping(target = "hasNextPage", expression = "java(page.hasNext())")
+    @Mapping(target = "lastPageNumber", source = "totalPages")
+    @Mapping(target = "totalElements", source = "totalElements")
+    PageableResponse<LanguageResponse> toPageableResponse(Page<Language> page);
+
+    @Named("getById")
+    default LanguageStatus getById(Integer id) {
+        return LanguageStatus.getStatusById(id);
+    }
+
+    @Named("getLanguageList")
+    default List<LanguageResponse> getLanguageList(List<Language> languages) {
+        return languages.stream().map(this::toResponse).toList();
+    }
+
 }
