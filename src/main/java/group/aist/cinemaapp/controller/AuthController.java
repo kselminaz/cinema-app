ee8095388fa215f51b92cd790bf584429bcff334
alsu-ismayilzada <ismayilzadealsu@gmail.com>
2024-07-23 13:34:51 +0400

    feat: adding user authentication and authorization

--- src/main/java/group/aist/cinemaapp/controller/AuthController.java
+++ src/main/java/group/aist/cinemaapp/controller/AuthController.java
@@ -8,10 +8,7 @@ import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/api")
@@ -55,4 +52,13 @@ public class AuthController {
             return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body("Failed to create user: " + e.getMessage());
         }
     }
+    @PostMapping("/refresh-token")
+    public TokenResponse getAccessTokenByRefreshToken(@RequestBody String refreshToken) {
+        try {
+            return authService.getAccessTokenByRefreshToken(refreshToken);
+        } catch (Exception e) {
+            throw e;
+        }
+    }
+
 }
